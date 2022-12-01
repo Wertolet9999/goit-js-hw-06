1c90c645a83f6f5d0886b1ba7e099f142387432d
Wertolet9999 <wertolet9999@gmail.com>
2022-12-01 18:01:38 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients')
+for (let i = 0; i < ingredients.length; i++) {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = ingredients[i];
+  ingredientsEl.append(liEl);
+}
