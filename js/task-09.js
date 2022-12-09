8b19380a1732956975b841b228729b46da81f715
Wertolet9999 <wertolet9999@gmail.com>
2022-12-09 12:07:05 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
+const bodyRef = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const selectColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  bodyRef.style.backgroundColor = selectColor;
+  textEl.textContent = selectColor;
 }
+btnEl.addEventListener('click', getRandomHexColor)
