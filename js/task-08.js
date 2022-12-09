3ec3e25f034de131dfaa35ee4fad167af3f1d11c
Wertolet9999 <wertolet9999@gmail.com>
2022-12-09 11:01:37 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    let { email, password } = event.target.elements;
+    if (email.value === "" || password.value === "") {
+        alert('Всі поля повинні бути заповнені')
+    }
+    email = email.value;
+    password = password.value;
+    event.target.reset()
+    console.log({Email:email, Password:password})
+})
